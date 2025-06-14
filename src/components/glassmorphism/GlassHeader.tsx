40e4b938da3951004728b14bc7ea0648f1094338
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-06-15 01:59:19 +0400

    Fix GlassHeader by adding useState import

--- src/components/glassmorphism/GlassHeader.tsx
+++ src/components/glassmorphism/GlassHeader.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Search, Bell, ChevronDown, Sun, Moon, Download, Settings, Wifi, WifiOff } from 'lucide-react';
 import { GlassCard } from './GlassCard';
 import { GlassButton } from './GlassButton';
