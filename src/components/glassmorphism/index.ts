babd98572a3107679faaf061fa85f4e0e67fbd56
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-06-15 09:26:11 +0400

    Update glassmorphism index to export GlassSubNav component

--- src/components/glassmorphism/index.ts
+++ src/components/glassmorphism/index.ts
@@ -6,6 +6,7 @@ export { GlassSidebar } from './GlassSidebar';
 export { GlassHeader } from './GlassHeader';
 export { GlassChart } from './GlassChart';
 export { GlassSummaryCard } from './GlassSummaryCard';
+export { GlassSubNav } from './GlassSubNav';
 export { default as GlassFilterBar } from './GlassFilterBar';
 
 // Re-export types
